73406464fe017aa3c99ec4eeb9884c6764f7417f
tobias <you@example.com~>
2021-03-15 12:55:31 +0100

    createuser

--- app/services/account.js
+++ app/services/account.js
@@ -1,4 +1,8 @@
-
+/**
+ * Sign in user
+ * @param {String} username 
+ * @param {String} password 
+ */
 function signInUser(username, password) {
     console.log('signing in...');
     var myHeaders = new Headers();
@@ -21,6 +25,7 @@ function signInUser(username, password) {
         .then(result => {
             if (result.status === 200) {
                 console.log('Success logging in:'+ result.text())
+                return true;
             }
             else{
                 console.log('login failed: '+result.body);
@@ -30,4 +35,30 @@ function signInUser(username, password) {
         .catch(error => console.log('error', error));
 }
 
-export {signInUser}
+function createUser(username, password) {
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let raw = JSON.stringify({
+        "username": username,
+        "password": password
+    })
+
+    let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("https://fjellturchallenge-backend-dev.herokuapp.com/api/v1/accounts/register", requestOptions)
+        .then(result => {
+            if (result.status === 200) {
+                console.log('Success')
+                navigation.navigate("Sign In")
+            }
+        })
+        .catch(error => console.log('error', error));
+}
+
+export {signInUser, createUser}
