bc10d03589abab74790988b5449a6bcf6a713fce
TorjusFS <torjus.schaathun@gmail.com>
2021-04-29 13:35:25 +0200

    make map cover entire screen

--- app/components/ActiveMap.js
+++ app/components/ActiveMap.js
@@ -12,8 +12,8 @@ import {
 const styles = StyleSheet.create({
     container: {
         ...StyleSheet.absoluteFillObject,
-        height: 400,
-        width: 400,
+        height: "100%",
+        width: "100%",
         // justifyContent: 'flex-end',
         alignItems: 'center',
     },
